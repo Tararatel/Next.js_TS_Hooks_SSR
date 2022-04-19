09dafcb707b4416852cf7a1a6b61fa44a3dba7d7
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-04-19 13:46:42 +0300

    Добавление роутинга и линков

--- context/app.context.tsx
+++ context/app.context.tsx
@@ -4,16 +4,17 @@ import { TopLevelCategory } from '../interfaces/page.interface';
 
 export interface IAppContext {
 	menu: MenuItem[];
-	firstcategory: TopLevelCategory;
+	firstCategory: TopLevelCategory;
 	setMenu?: (newMenu: MenuItem[]) => void;
 }
 
-export const AppContext = createContext<IAppContext>({ menu: [], firstcategory: TopLevelCategory.Courses });
+export const AppContext = createContext<IAppContext>({ menu: [], firstCategory: TopLevelCategory.Courses });
 
-export const AppContextProvider = ({ menu, firstcategory, children }: PropsWithChildren<IAppContext>): JSX.Element => {
+export const AppContextProvider = ({ menu, firstCategory, children }: PropsWithChildren<IAppContext>): JSX.Element => {
 	const [menuState, setMenuState] = useState<MenuItem[]>(menu);
 	const setMenu = (newMenu: MenuItem[]) => {
 		setMenuState(newMenu);
 	};
-	return <AppContext.Provider value={{ menu: menuState, firstcategory, setMenu }}>{children}</AppContext.Provider>;
+
+	return <AppContext.Provider value={{ menu: menuState, firstCategory, setMenu }}>{children}</AppContext.Provider>;
 };
