ccf9c63c555afcc2a80e2f533576aeab09ab0a5c
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-04-20 16:48:21 +0300

    Сделана страница продукта, использован useReducer

---
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -0,0 +1,57 @@
+import { Advantages, HhData, Htag, Sort, Tag } from '../../components';
+import { TopPageComponentProps } from './TopPageComponent.props';
+import styles from './TopPageComponent.module.css';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import { SortEnum } from '../../components/Sort/Sort.props';
+import { useReducer } from 'react';
+import { sortReducer } from './sort.reducer';
+
+export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
+	const [{ products: sortedProducts, sort }, dispatchSort] = useReducer(sortReducer, {
+		products,
+		sort: SortEnum.Rating,
+	});
+
+	const setSort = (sort: SortEnum) => {
+		dispatchSort({ type: sort });
+	};
+
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.title}>
+				<Htag tag="h1">{page.title}</Htag>
+				{products && (
+					<Tag color="grey" size="medium">
+						{products.length}
+					</Tag>
+				)}
+				<Sort sort={sort} setSort={setSort} />
+			</div>
+			<div>{sortedProducts && sortedProducts.map((p) => <div key={p._id}>{p.title}</div>)}</div>
+
+			<div className={styles.hhTitle}>
+				<Htag tag="h2">Вакансии - {page.category}</Htag>
+				<Tag color="red" size="medium">
+					hh.ru
+				</Tag>
+			</div>
+
+			{firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+
+			{page.advantages && page.advantages.length > 0 && (
+				<>
+					<Htag tag="h2">Преимущества</Htag>
+					<Advantages advantages={page.advantages} />
+				</>
+			)}
+
+			{page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
+			<Htag tag="h2">Получаемые навыки</Htag>
+			{page.tags.map((t) => (
+				<Tag color="primary" key={t}>
+					{t}
+				</Tag>
+			))}
+		</div>
+	);
+};
