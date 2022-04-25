2a1042e8fb54438a2751f222688cc8ae8e5749b3
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-04-25 16:59:35 +0300

    Основной функционал реализован. Сделана форма отправки отзыва. Внесены правки

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { GetStaticProps } from 'next';
 import { useEffect, useState } from 'react';
 import { Button, Htag, P, Rating, Tag, Input, Textarea, Search } from '../components';
+import { API } from '../helpers/api';
 import { MenuItem } from '../interfaces/menu.interface';
 import { withLayout } from '../layout/Layout';
 
@@ -42,7 +43,7 @@ export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps = async () => {
 	const firstCategory = 0;
-	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+	const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
 		firstCategory,
 	});
 	return {
