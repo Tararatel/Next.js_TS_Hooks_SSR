e025963e4045094dfa9054aa521435d5f3ac39fa
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-05-09 17:06:58 +0300

    // category

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -7,9 +7,9 @@ import { TopLevelCategory } from '../interfaces/page.interface';
 
 export const firstLevelMenu: FirstLevelMenuItem[] = [
 	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+	// { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+	// { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+	// { route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
 ];
 
 export const priceRu = (price: number): string =>
